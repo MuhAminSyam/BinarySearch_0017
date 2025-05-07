27c4ad3c6063bc925ff9fcc1cbedab15e7242709
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-07 20:05:11 +0700

    menambahkan logika input jumlah elemen array

---
+++ BinarySearch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int elemen[10];
+int n;
+int i;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> n;
+
+        if (n <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nJumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
+    }
